b6d1fbd155fa545a5286b404ed993fcb8842f055
UncleBo <bugrasukas97@gmail.com>
2020-02-20 22:02:28 -0700

    New files

--- server/index.js
+++ server/index.js
@@ -10,19 +10,33 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 app.use(router);
 
 io.on("connection", socket => {
-  console.log("We have a new connection!!!");
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-    const error = true;
+    if (error) return callback(error);
 
-    // if (error) {
-    //   callback({ error: "error" });
-    // }
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+
+    socket.join(user.room);
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
   socket.on("disconnect", () => {
